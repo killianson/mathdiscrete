c329ef5e4c4a6762cd397bde34b2b3b09719ea52
killianson <killian.j.sonna@gmail.com>
2022-05-08 16:56:09 +0200

    updata linear

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import csv
 
 
-def pageRankLinear(A, alpha, v):
+def pageRankLinear(A, alpha=0.9, v=None):
     """
     pre :
     A : Une matrix numpy, une matrice d'adjacence d'un graphe dirigé, pondéré et régulier G
@@ -11,7 +11,8 @@ def pageRankLinear(A, alpha, v):
     post :
     vecteur x contenant les scores d'importance des noeuds ordonnés dans le même ordre que la matrice d'adjacence
     """
-    def get_stochastic_matrix(A):
+    if v is None: v = np.array([1/len(A)] * len(A))
+    def get_prob_matrix(A):
         """
         Retourne la matrice stochastique (la somme des éléments de chaque ligne vaut 1)
         """
@@ -19,37 +20,20 @@ def pageRankLinear(A, alpha, v):
         P = np.zeros((n, n))
         for i in range(n):
             for j in range(n):
-                if A[i,j] != 0:
-                    P[i, j] = A[i, j]/ np.sum(A[i])
-                else : 
-                    P[i,j]=0
+                if A[i, j] != 0:
+                    P[i, j] = A[i, j] / np.sum(A[i])
+                else:
+                    P[i, j] = 0
         return P
 
-    def update_until_converge(P, v):
-        """
-        Produit entre P et v jusqu'à ce que v converge
-        """
-        new_vector = np.matmul(P, v)
-        loop = True
-        while loop:
-            v = new_vector
-            new_vector = np.matmul(P, new_vector)
-            for i in range(len(v)):
-                if abs(v[0] - new_vector[0]) < 10e-10 and abs(v[1] - new_vector[1]) < 10e-10 and abs(
-                        v[0] - new_vector[0]) < 10e-10:
-                    loop = False
-        return new_vector
+    # Matrice de proba : la somme des éléments de chaque ligne vaut 1
+    P = get_prob_matrix(A)
 
-    # Matrice stochastic : la somme des éléments de chaque ligne vaut 1
-    P = get_stochastic_matrix(A)
-
-    # Mettre à jour le vecteur de personnalisation jusqu'à convergence
-    return update_until_converge(P.T, v)
+    new_matrix = np.transpose((np.identity(len(P)) - alpha * P))
+    return np.linalg.solve(new_matrix, (1 - alpha) * v)
 
 
-
-
-def pageRankPower(A, alpha, v):
+def pageRankPower(A, alpha=0.9, v=None):
     """
     pre :
     A : Une matrix numpy, une matrice d'adjacence d'un graph dirigé, pndérer et régulier G
@@ -58,7 +42,8 @@ def pageRankPower(A, alpha, v):
     post : 
     vecteur x contenant les scores d'importance des noeuds ordonnés dans le même ordre que la matrice d'adjacence
     """
-    def ToProb(A):#détermine la matrice de probabilité de la matrice A
+    if v is None: v = np.array([1 / len(A)] * len(A))
+    def ToProb(A):  # détermine la matrice de probabilité de la matrice A
         b = []
         for i in range(len(A)):
             sum = 0
@@ -72,24 +57,28 @@ def pageRankPower(A, alpha, v):
                     b[i].append(A[i, j] / sum)
         p = np.array(b)
         return p
-    def vecteur_taille(A,vect = v):#ajustement de la taille du vecteur pour certains test
+
+    def vecteur_taille(A, vect=v):  # ajustement de la taille du vecteur pour certains test
         l = []
         for i in range(len(A[0])):
             l.append(vect[i])
         v = np.array(l)
         return v.T
-    def M_chap(P = ToProb(A), a = alpha):#Détermine G
+
+    def M_chap(P=ToProb(A), a=alpha):  # Détermine G
         E = np.ones((len(P[0]), len(P)))
-        return ((alpha*P) + (1-alpha)*(E/(len(P))))
-    def new_v2(P,v):#détermine p@v, à refaire car on peut pas utiliser numpy pour ça je pense 
-        return P@v
+        return ((alpha * P) + (1 - alpha) * (E / (len(P))))
+
+    def new_v2(P, v):  # détermine p@v, à refaire car on peut pas utiliser numpy pour ça je pense
+        return P @ v
+
     M2 = M_chap()
     v1 = vecteur_taille(A, v)
     max_iter = 100
     psy = 0.005
     for i in range(max_iter):
-        v2 = new_v2(M2,v1)
-        if np.linalg.norm(v2 - v1,1) < psy:
+        v2 = new_v2(M2, v1)
+        if np.linalg.norm(v2 - v1, 1) < psy:
             break
         v1 = v2
     return v1
@@ -101,21 +90,25 @@ def main():
     contenant la matrice d’adjacence A du graphe présenté ci-dessous (séparez vos valeurs par
     une virgule et chaque ligne de la matrice par un passage à la ligne), qui exécute le calcul de
     PageRank (via vos méthodes pageRankPower et pageRankLinear) et imprime les résultats
-    """ 
-    #lit le fichier csv et crée un numpy array avec les données
-    with open('matrice.csv', mode = 'r') as file: 
+    """
+    # lit le fichier csv et crée un numpy array avec les données
+    with open('matrice.csv', mode='r') as file:
         file = csv.reader(file)
-        mat = np.array([lines for lines in file ], int)
+        mat = np.array([lines for lines in file], int)
 
-    #vecteur de personnalisation 
+    # vecteur de personnalisation
     v = np.array([0.1179, 0.1674, 0.2309, 0.1784, 0.0009, 0.1548, 0.049, 0.0315, 0.0105, 0.0587])
 
-    #alpha
+    # alpha
     a = 0.9
 
-    #execution + print
+    # execution + print
 
-    print("\nPage Rank Linear results : \n", pageRankLinear(mat,a,v), "\n")
-    print("Page Rank Power results : \n", pageRankPower(mat,a,v), "\n")
+    print("\nPage Rank Linear results : \n", pageRankLinear(mat, a, v), "\n")
+    print("Page Rank Power results : \n", pageRankPower(mat, a, v), "\n")
     pass
+
+
 main()
+
+
